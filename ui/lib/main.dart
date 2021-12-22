07fdaa98f3a37b5b31b0957b796dc56695adafd1
Taylor Ninesling <taylor.ninesling@protonmail.com>
2021-12-22 01:34:10 -0800

    A good start to neumorphism

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,21 +1,6 @@
-import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:ui/providers/workout_form.dart';
-import 'package:ui/screens/create_exercise.dart';
-import 'package:ui/screens/create_workout.dart';
-import 'package:ui/screens/home.dart';
-
-void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => WorkoutFormState())
-      ],
-      child: MyApp()
-    )
-  );
-}
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class MyRoutes {
   static String get home => '/';
@@ -23,28 +8,143 @@ class MyRoutes {
   static String get newWorkout => '/workouts/new';
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
 
-  // https://pub.dev/packages/beamer#quick-start
-  final beamerDelegate = BeamerDelegate(
-    locationBuilder: RoutesLocationBuilder(
-      routes: {
-        MyRoutes.home: (context, state, data) => const HomeScreen(title: "Home sweet home"),
-        MyRoutes.newExercise: (context, state, data) => const CreateExerciseScreen(),
-        MyRoutes.newWorkout: (context, state, data) => const CreateWorkoutScreen()
-      }
-    ),
-    notFoundRedirectNamed: MyRoutes.home,
-  );
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationParser: BeamerParser(),
-      routerDelegate: beamerDelegate,
-      backButtonDispatcher: BeamerBackButtonDispatcher(delegate: beamerDelegate),
+    return NeumorphicApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Heath',
+      themeMode: ThemeMode.light,
+      // Color palettes: https://api.flutter.dev/flutter/material/Colors-class.html
+      theme: NeumorphicThemeData(
+        baseColor: Colors.indigo.shade100,
+        depth: 2,
+        lightSource: LightSource.topLeft,
+        iconTheme: IconThemeData(
+          color: Colors.indigo.shade300,
+        ),
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            color: Colors.indigo.shade900,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+          headline2: TextStyle(
+            color: Colors.indigo.shade800,
+            fontSize: 24,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.indigo.shade300,
+            fontSize: 16,
+          )
+        )
+      ),
+      darkTheme: const NeumorphicThemeData(
+        baseColor: Color(0xFF3E3E3E),
+        lightSource: LightSource.topLeft,
+        defaultTextColor: Color(0xFFFFFFFF),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: NeumorphicTheme.baseColor(context),
+      floatingActionButton: const NewWorkoutButton(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 8),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Container(
+                  child: Text(
+                    "Hey!\nIt's time to work out.",
+                    style: NeumorphicTheme.currentTheme(context).textTheme.headline1
+                  ),
+                  margin: const EdgeInsets.symmetric(vertical: 16),
+                ),
+                Container(
+                  child: Text(
+                    "Here's today's plan",
+                    style: NeumorphicTheme.currentTheme(context).textTheme.headline2,  
+                  ),
+                  margin: const EdgeInsets.symmetric(vertical: 16)
+                ),
+                const ExerciseCard(exercise: "Deadlift: 5x5 @ 210lbs"),
+                const ExerciseCard(exercise: "Overhead Press: 5x5 @ 70lbs"),
+                const ExerciseCard(exercise: "Leg Curls: 5x10 @ 110lbs"),
+                const ExerciseCard(exercise: "Leg Extensions: 5x10 @ 110lbs"),
+              ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+            RotatedBox(
+              child: NeumorphicText(
+                "EXERCISE",
+                style: const NeumorphicStyle(
+                  depth: 1,
+                  intensity: 0.6,
+                  lightSource: LightSource.bottomLeft
+                ),
+                textStyle: NeumorphicTextStyle(fontSize: 64, fontWeight: FontWeight.bold),
+              ),
+              quarterTurns: 1,
+            )
+          ]
+        )
+      )
+    );
+  }
+}
+
+class NewWorkoutButton extends StatelessWidget {
+  const NewWorkoutButton({Key? key}) : super(key : key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicFloatingActionButton(
+      child: const Center(
+        child: Icon(
+          Icons.fitness_center_rounded,
+          size: 30,
+        )
+      ),
+      onPressed: () {
+        Fluttertoast.showToast(msg: "Pressed");
+      },
+      style: NeumorphicStyle(
+        color: NeumorphicTheme.baseColor(context),
+      ),
+    );
+  }
+}
+
+class ExerciseCard extends StatelessWidget {
+  final String exercise;
+
+  const ExerciseCard({Key? key, required this.exercise}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Neumorphic(
+      child: Text(
+        exercise,
+        style: NeumorphicTheme.currentTheme(context).textTheme.bodyText1,
+      ),
+      padding: const EdgeInsets.all(8),
+      margin: const EdgeInsets.symmetric(vertical: 8),
     );
   }
 }
