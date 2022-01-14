6c267819b37058b12e4ab3fdf9f76b83d4c2d880
Taylor Ninesling <taylor.ninesling@protonmail.com>
2022-01-14 00:24:27 -0800

    Turn seeder back on

--- api/seeder/sql/01-SeedIngredientsTable.sql
+++ api/seeder/sql/01-SeedIngredientsTable.sql
@@ -3,4 +3,5 @@ INSERT INTO ingredients
 VALUES
   ('00193af6-1a16-4bb6-bee0-6c7d282b4ddc', 'Ingredient1', 650, 17, 25, 10, 25.3),
   ('3c9d743c-946a-4e1d-b50a-b236dfa4feea', 'Ingredient2', 300, 28, 0, 3, 38),
-  ('c4adb381-73ea-489b-91a9-4d023f541d0a', 'Ingredient3', 420, 20, 20, 20, 15.3);
+  ('c4adb381-73ea-489b-91a9-4d023f541d0a', 'Ingredient3', 420, 20, 20, 20, 15.3)
+ON CONFLICT DO NOTHING;
