48b8f5e85725f2fa205afebec765feb99746b8d1
Taylor Ninesling <taylor.ninesling@gmail.com>
2022-04-15 23:35:44 -0700

    Trimmed some dead code

--- ui/lib/models/trend.dart
+++ ui/lib/models/trend.dart
@@ -1,5 +1,4 @@
 import 'package:openapi/api.dart' as openapi;
-import 'package:ui/dtos/trend.dart';
 
 class Trend {
   final String name;
@@ -7,12 +6,6 @@ class Trend {
   final Line line;
 
   Trend({required this.name, required this.points, required this.line});
-
-  factory Trend.fromDto(TrendDto dto) => Trend(
-        name: dto.name,
-        points: dto.points.map(Point.fromDto).toList(),
-        line: Line.fromDto(dto.line),
-      );
 
   factory Trend.fromOpenapi(openapi.Trend t) => Trend(
         name: t.name,
@@ -28,9 +21,6 @@ class Point {
 
   Point({required this.x, required this.y, required this.label});
 
-  factory Point.fromDto(PointDto dto) =>
-      Point(x: dto.x, y: dto.y, label: dto.label);
-
   factory Point.fromOpenapi(openapi.Point p) => Point(
         x: p.x,
         y: p.y,
@@ -43,9 +33,6 @@ class Line {
   final double intercept;
 
   Line({required this.slope, required this.intercept});
-
-  factory Line.fromDto(LineDto dto) =>
-      Line(slope: dto.slope, intercept: dto.intercept);
 
   factory Line.fromOpenapi(openapi.Line l) => Line(
         slope: l.slope,
